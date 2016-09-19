674025982d96412f92db250bbfbf88bcf9b94024
Jan Drenkhahn <jan-dennisd@outlook.at>
2016-09-19 21:45:24 +0200

    Verbesserungen an der Daten Ausgabe und Ausgabe in eine Datei. Preis Ausgabe stimmt noch nicht

--- restaurant.py
+++ restaurant.py
@@ -8,7 +8,7 @@ while True:
     dish = raw_input("Bitte gib die Hauptspeise ein: ")
     price = raw_input("Gib bitte den Preis in Euro ein: ")
 
-    print "Die Speise " + dish + " Kostet " + price + "€"
+    print "Das  Tagesgericht " + dish + " Kostet " + price + "€"
 
     new = raw_input("Möchtest du eine weitere Speise erfassen? Ja/Nein: ")
 
@@ -19,5 +19,12 @@ while True:
 
 print "Das Tagesmenü lautet heute %s" % menu
 
-with open("menue.txt", "w+") as todo_file:
-    print "Unser heutiges Tagesmenü:"
+with open("menu.txt", "w+") as restaurant_file:
+    print "Unsere heutigen Tagesmenüs:"
+    restaurant_file.write("Unsere heutigen Tagesmenüs:\n")
+    for item in menu:
+        print item + " - " + price + "€"
+        restaurant_file.write("- " + item + " - " + price + "€" + "\n")
+    restaurant_file.write("\n")
+
+
